4a98669f86d046f98430fbbdfec6a3e7c7daaf86
Mycah <mycahdt@udel.edu>
2025-01-30 13:40:42 -0500

    lesson 3 v binding

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://sheecsocks.com/blogs/being-chic/types-of-socks?srsltid=AfmBOorDFhX_xuJiTFiKXUPeW8w-74XmJBH9Xv60HqE9SRUsNh8_j44l'
         }
     }
 })
